4f7c3ae2fb21afe13f2a835f145e653a75e5f833
j-c-cook <jack.cook@okstate.edu>
2020-03-07 15:54:03 -0600

    add in foopackage, setup, and reqs

---
+++ FooPackage/rectangle_.pyx
@@ -0,0 +1,56 @@
+# rectangle.pyx
+cdef extern from "rectangle.h" namespace "shapes":
+    cdef cppclass Rectangle:
+        Rectangle(int, int, int, int)
+        int x0, y0, x1, y1
+        int getLength()
+        int getHeight()
+        int getArea()
+        void move(int, int)
+
+cdef class PyRectangle:
+    cdef Rectangle *c_rect      # hold a C++ instance which we're wrapping
+    def __cinit__(self, int x0, int y0, int x1, int y1):
+        self.c_rect = new Rectangle(x0, y0, x1, y1)
+    def __dealloc__(self):
+        del self.c_rect
+    def getLength(self):
+        return self.c_rect.getLength()
+    def getHeight(self):
+        return self.c_rect.getHeight()
+    def getArea(self):
+        return self.c_rect.getArea()
+    def move(self, dx, dy):
+        self.c_rect.move(dx, dy)
+
+    # Attribute access
+    @property
+    def x0(self):
+        return self.c_rect.x0
+    @x0.setter
+    def x0(self, x0):
+        self.c_rect.x0 = x0
+
+    # Attribute access
+    @property
+    def x1(self):
+        return self.c_rect.x1
+    @x1.setter
+    def x1(self, x1):
+        self.c_rect.x1 = x1
+
+    # Attribute access
+    @property
+    def y0(self):
+        return self.c_rect.y0
+    @y0.setter
+    def y0(self, y0):
+        self.c_rect.y0 = y0
+
+    # Attribute access
+    @property
+    def y1(self):
+        return self.c_rect.y1
+    @y1.setter
+    def y1(self, y1):
+        self.c_rect.y1 = y1
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+extensions = [
+    Extension(
+        name='FooPackage.rectangle',  # name/path to generated so file
+        sources=['FooPackage/rectangle.cpp', 'FooPackage/rectangle_.pyx'],  # the files that go into building the .so
+        include_dirs=[numpy.get_include()],
+        language='c++')  # could put comma and list another extension
+]
+
+setup(name='cythonpack',
+      version='0.0.1',
+      author='Jack C. Cook',
+      author_email='jack.cook@okstate.edu',
+      include_package_data=True,
+      packages=['FooPackage'],  # packages = [P1,P2] could contain multiple packages
+      ext_modules=extensions,
+      cmdclass={'build_ext': build_ext})
